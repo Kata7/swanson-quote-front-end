3d099717742674ff07846570ed60c7a4a9c2e4d4
Patrick Biffle <pbiffle@gmail.com>
2019-02-25 14:59:02 -0700

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Ron Swanson Quote Generator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appHeader'
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 1.5em;
+    color: red;
+  }
+</style>
